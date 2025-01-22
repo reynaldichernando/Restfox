b9523f6ff026d6da4a103043bfb84e2525b89d53
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-01-22 13:53:41 +0700

    temp comment tagging

--- packages/ui/vite-plugin-revision.js
+++ packages/ui/vite-plugin-revision.js
@@ -7,10 +7,10 @@ function execShellCommandSync(cmd) {
 export const ViteRevisionPlugin = (mode) => ({
     name: 'vite-revision',
     config() {
-        const latestTag = mode === 'development' ? 'development' : execShellCommandSync('git describe --tags --abbrev=0')
-        const latestCommitHash = mode === 'development' ? 'development' : execShellCommandSync('git rev-parse HEAD')
+        // const latestTag = mode === 'development' ? 'development' : execShellCommandSync('git describe --tags --abbrev=0')
+        // const latestCommitHash = mode === 'development' ? 'development' : execShellCommandSync('git rev-parse HEAD')
 
-        process.env.VITE_GIT_TAG = latestTag
-        process.env.VITE_GIT_COMMIT_HASH = latestCommitHash.substring(0, 7)
+        // process.env.VITE_GIT_TAG = latestTag
+        // process.env.VITE_GIT_COMMIT_HASH = latestCommitHash.substring(0, 7)
     }
 })
